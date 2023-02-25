e4a34b9ffee03112f431565bd4d642a4ac5e3f44
farhanthegreat <farhantuber@gmail.com>
2023-02-25 06:53:11 +0600

    git added

---
+++ js/arrow.js
@@ -0,0 +1,37 @@
+//function declaration
+// function add(first, second){
+//     const total = first + second;
+//     return total;
+// }
+
+//function expression
+const add1 = function addBG(first, second){
+    const total = first + second;
+    return total;
+}
+
+//function expression with anonymous function
+const add2 = function (first, second){
+    const total = first + second;
+    return total;
+}
+
+
+function add(first, second){
+    const total = first + second;
+    return total;
+}
+
+function add4(first, second){
+    return first + second;
+}
+
+const add5 = function(first,second){
+    return first + second;
+}
+
+//arrow function
+const add6 = (first, second) => first + second;
+
+const result = add(10,20);
+// console.log(result);
---
+++ js/template.js
@@ -0,0 +1,7 @@
+const nums = [55, 25, 01, 11, 10, 9];
+const a = 10;
+const b = 20;
+const total = `Total of ${a} and ${b} is = ${a+b}`;
+console.log(total);
+const sum = `Sum of ${a} and ${nums.length} is = ${a+nums.length}`;
+console.log(sum);
